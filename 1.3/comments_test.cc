d4afd9134be6e9bc145611f58457f20e025e13e9
DinoPy <78703794+DinoPy@users.noreply.github.com>
2023-08-23 19:09:16 +0000

    up to page 18, I/O, while, for, if,  errors

---
+++ 1.3/comments_test.cc
@@ -0,0 +1,5 @@
+/*
+* commen pairs /*     */ connot nest.
+* "cannot nest" is considered source code
+* as is the rest of the program
+*/
---
+++ 1.4/eg1.15.3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << Update master. << std::endl;
+    return 0;
+}
+
+/*
+eg1.15.3.cc: In function ‘int main()’:
+eg1.15.3.cc:5:18: error: ‘Update’ was not declared in this scope
+    5 |     std::cout << Update master. << std::endl;
+      | 
+*/
