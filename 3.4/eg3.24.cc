e4427efdc9c64da0b5948f39c35367f33e16a350
DinoPy <78703794+DinoPy@users.noreply.github.com>
2023-09-11 04:57:41 +0000

    up to exercices for 3.4

---
+++ 3.4/eg3.24.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector; using std::next;
+using std::cin; using std::cout; using std::endl;
+
+int main() {
+    vector<int> ivec;
+    int input;
+    while (cin >> input)
+        ivec.push_back(input);
+    auto length = ivec.size();
+    cout << "vector length " << length << endl;
+    // for (unsigned i = 0; i < length; i += 2) {
+    //     if (i == length - 1 && (i + 1) % 2 > 0)
+    //         cout << ivec[i] << " has no pair" << endl;
+    //     else 
+    //         cout << ivec[i] + ivec[i+1] << endl;
+    // }
+    for (auto beg = ivec.begin(), end = ivec.end(); beg < end; beg += 2 ) {
+        if (next(beg) != end)
+            cout << *beg + *(next(beg)) << endl;
+        else 
+            cout << *beg << " has no pair" << endl;
+    }
+
+
+
+    // for (unsigned i = 0; i <= length/2; i += 1) {
+    //     if (i * 2 + 1 == length)
+    //         cout << ivec[i] << " has no pair" << endl;
+    //     else
+    //         cout << ivec[i] + ivec[length - i - 1] << endl;
+    // }
+    for(auto beg = ivec.begin(), end = ivec.end() - 1; beg != end; ++beg, --end) {
+        cout << *beg + *end << endl;
+    }
+    return 0;
+}
