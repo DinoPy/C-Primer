e00cbe1c67f99323a728caa730edc998b7aa7a1f
DinoPy <78703794+DinoPy@users.noreply.github.com>
2024-10-20 04:53:29 +0000

    done with exercises for 12.2.1

--- 12/2/24.cpp
+++ 12/2/24.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 
 
 int main (int argc, char *argv[]) {
     std::string input = "";
     std::cin >> input;
     const size_t length = input.size();
-    /*char c_str[length] = (input.c_str());*/
-    std::cout << input << std::endl;
+    char* c_str= new char[length]; 
+    strcpy(c_str,input.c_str());
+    std::cout << c_str << std::endl;
+    delete[] c_str;
     return 0;
 }
+
+// Given that there was no mention again using a regular string as a helper 
+// I am using a string to read the contents of the input 
+// then copy the results in a c string.
---
+++ 12/2/25.cpp
@@ -0,0 +1,9 @@
+int main(int argc, char const *argv[])
+{
+    int *pa = new int[10];
+    delete[] pa;
+    return 0;
+}
+
+// given that pa is a dynamically allocated array it has to be deleted
+// using delete[]
